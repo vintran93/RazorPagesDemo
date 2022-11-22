a64284055e4f5d556308d8357e938840aa778709
Vincent Tran <vincentuong@yahoo.com>
2022-11-22 00:26:52 -0600

    Add project files.

---
+++ RazorPagesUI/Models/AddressModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesUI.Models
+{
+	public class AddressModel
+	{
+		public string? StreetAddress { get; set; }
+		public string? City { get; set; }
+		public string? State { get; set; }
+		public string? ZipCode { get; set; }
+	}
+}
---
+++ RazorPagesUI/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesUI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        /*public int Id { get; set; }*/
+        public string City { get; set; }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(City))
+            {
+                City = "The Web";
+            }
+        }
+    }
+}
